33eed665652897e370d0d79489a0d93f2cb1cb23
MarionLz <marion.lauzier@ecomail.fr>
2024-05-13 13:36:40 +0200

    adding nulterminate function, now the program can exec a cmd with args like ls -l

--- src/parsing.c
+++ src/parsing.c
@@ -106,6 +106,24 @@ t_node	*parse_exec(char **start_scan, char *end_input)
 	return (node);
 }
 
+t_node	*nulterminate(t_node *tree)
+{
+	t_exec_node	*ex_node;
+	int			i;
+
+	if (tree->type == EXEC)
+	{
+		ex_node = (t_exec_node *)tree;
+		i = 0;
+		while (ex_node->args[i])
+		{
+			*ex_node->end_args[i] = 0;
+			i++;
+		}
+	}
+	return (tree);
+}
+
 t_node	*parse_input(char *input)
 {
 	char	*end_input;
@@ -113,7 +131,5 @@ t_node	*parse_input(char *input)
 
 	end_input = input + ft_strlen(input);
 	tree = parse_exec(&input, end_input);
-	replace_whitespaces();
-	nulterminate pour remplacer tous les espaces par des \0
-	return (tree);
+	return (nulterminate(tree));
 }
