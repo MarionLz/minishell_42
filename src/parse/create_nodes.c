9e6666e21f9f0499e199133c9370aa8e269d581c
MarionLz <marion.lauzier@ecomail.fr>
2024-05-13 17:08:31 +0200

    adding parse_pipe & create_pipe_node

--- src/parse/create_nodes.c
+++ src/parse/create_nodes.c
@@ -4,7 +4,7 @@
 exec_node->args[i]: pointer to the beginning of the command or argument.
 exec_node->end_args[i]: pointer to the character following the command or argument.*/
 
-t_node	*create_pipe_node(char *left, char *right)
+t_node	*create_pipe_node(t_node *left, t_node *right)
 {
 	t_pipe_node	*pipe_node;
 
@@ -31,8 +31,9 @@ t_node	*create_exec_node(char **start_scan, char *end_input)
 		return (NULL);
 	ft_memset(exec_node, 0, sizeof(*exec_node));
 	exec_node->type = EXEC;
-	while (*start_scan < end_input && get_token(start_scan, end_input, &start_token, &end_token) == EXEC)
+	while (*start_scan < end_input && !insight_input(start_scan, end_input, "|"))
 	{
+		get_token(start_scan, end_input, &start_token, &end_token);
 		exec_node->args[i] = start_token;
 		exec_node->end_args[i] = end_token;
 		i++;
